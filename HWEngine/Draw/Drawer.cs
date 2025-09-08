f5bd96f964ae29ce093eff4b2827303a6c597a8e
StarSignalAnt <starsigndev@gmail.com>
2025-09-08 16:06:37 +0100

    Add Drawer and Texture2D classes
    Introduces Drawer for drawing rectangles and Texture2D for loading and managing textures using OpenTK and ImageSharp. Also adds ImageSharp as a dependency in the project file.

---
+++ HWEngine/Draw/Drawer.cs
@@ -0,0 +1,27 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HWEngine.Draw
+{
+    public class Drawer
+    {
+
+        public Drawer()
+        {
+
+        }
+
+
+
+
+        public void Rect(Vector2 position,Vector2 size)
+        {
+
+        }
+
+
+    }
+
+}
---
+++ HWEngine/Texture/Texture2D.cs
@@ -0,0 +1,68 @@
+﻿using OpenTK.Graphics.OpenGL;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HWEngine.Texture
+{
+    public class Texture2D : IDisposable
+    {
+
+        public int Handle { get; private set; }
+
+        public Texture2D(string path)
+        {
+            // Generate texture handle
+            Handle = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2d,Handle);
+
+            // Load image with ImageSharp
+            using (var image = Image.Load<Rgba32>(path))
+            {
+                // Flip vertically for OpenGL coordinates
+                image.Mutate(x => x.Flip(FlipMode.Vertical));
+
+                // Copy pixel data to byte array
+                var pixels = new byte[image.Width * image.Height * 4];
+                image.CopyPixelDataTo(pixels);
+
+                // Upload to GPU
+
+                GL.TexImage2D(TextureTarget.Texture2d, 0, InternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, pixels);
+            }
+
+
+            // Set texture parameters
+            GL.TextureParameteri(Handle, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TextureParameteri(Handle, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+            GL.TextureParameteri(Handle, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TextureParameteri(Handle, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            // Generate mipmaps
+            GL.GenerateMipmap(TextureTarget.Texture2d);
+
+        }
+
+        public void Bind(int unit=0)
+        {
+            GL.ActiveTexture((TextureUnit)(((int)TextureUnit.Texture0)+ unit));
+            GL.BindTexture(TextureTarget.Texture2d, Handle);
+        }
+
+        public void Release(int unit=0)
+        {
+            GL.ActiveTexture((TextureUnit)(((int)TextureUnit.Texture0) + unit));
+            GL.BindTexture(TextureTarget.Texture2d, 0);
+        }
+
+        public void Dispose()
+        {
+            GL.DeleteTexture(Handle);
+        }
+
+    }
+}
